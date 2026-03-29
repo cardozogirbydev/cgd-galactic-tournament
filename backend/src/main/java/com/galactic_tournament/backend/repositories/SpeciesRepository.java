49ab4cec2df9191a591bfe4c34707a60c6b59977
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-29 18:24:40 -0400

    refactor(backend): reorder import statements in SpeciesRepository for consistency

--- backend/src/main/java/com/galactic_tournament/backend/repositories/SpeciesRepository.java
+++ backend/src/main/java/com/galactic_tournament/backend/repositories/SpeciesRepository.java
@@ -1,8 +1,9 @@
 package com.galactic_tournament.backend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 import com.galactic_tournament.backend.entities.Species;
 
