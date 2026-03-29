42845d5adf9f9cdc1a680298d380a44323c331cc
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-29 18:27:25 -0400

    feat(backend): add SpeciesController for managing species endpoints

---
+++ backend/src/main/java/com/galactic_tournament/backend/controllers/SpeciesController.java
@@ -0,0 +1,30 @@
+package com.galactic_tournament.backend.controllers;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.galactic_tournament.backend.dtos.SpeciesRequestDTO;
+import com.galactic_tournament.backend.dtos.SpeciesResponseDTO;
+import com.galactic_tournament.backend.services.SpeciesService;
+
+@RestController
+@RequestMapping("/api/species")
+@CrossOrigin(origins = "http://localhost:4200")
+public class SpeciesController {
+
+  @Autowired
+  private SpeciesService speciesService;
+
+  @GetMapping()
+  public List<SpeciesResponseDTO> getAll() {
+    return speciesService.getAll();
+  }
+
+  @PostMapping
+  public SpeciesResponseDTO create(@Valid @RequestBody SpeciesRequestDTO request) {
+    return speciesService.create(request);
+  }
+}
