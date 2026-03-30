8092db61848840f9093519d103eaecfef54bafc5
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-30 04:12:13 -0400

    feat(frontend): implement arena board component with fighter selection and combat logic

---
+++ frontend/src/app/features/arena/pages/arena-board/arena-board.ts
@@ -0,0 +1,84 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+import { SpeciesApi } from '@core/services/species-api';
+import { CombatsApi } from '@features/arena/services/combats-api';
+import type { CombatRequest } from '@core/models/combat.model';
+import type { SpeciesResponse } from '@core/models/species.model';
+
+@Component({
+  selector: 'app-arena-board',
+  imports: [],
+  templateUrl: './arena-board.html',
+})
+export class ArenaBoard {
+  private readonly speciesApi = inject(SpeciesApi);
+  private readonly combatsApi = inject(CombatsApi);
+
+  protected speciesResource = rxResource({
+    stream: () => this.speciesApi.getAll()
+  });
+
+  protected fighterOne = signal<SpeciesResponse | null>(null);
+  protected fighterTwo = signal<SpeciesResponse | null>(null);
+  protected winner = signal<SpeciesResponse | null>(null);
+  protected isFighting = signal(false);
+  protected canFight = computed(() => this.fighterOne() && this.fighterTwo() && !this.isFighting());
+
+  protected selectFighter(species:SpeciesResponse):void {
+    if (this.isFighting()) return;
+
+    if (this.fighterOne()?.id === species.id) {
+      this.fighterOne.set(null);
+      return;
+    }
+
+    if (this.fighterTwo()?.id === species.id) {
+      this.fighterTwo.set(null);
+      return;
+    }
+
+    if (!this.fighterOne()) {
+      this.fighterOne.set(species);
+      return;
+    }
+
+    if (!this.fighterTwo()) {
+      this.fighterTwo.set(species);
+      return;
+    }
+  }
+
+  protected startCombat():void {
+    const fighterOne = this.fighterOne();
+    const fighterTwo = this.fighterTwo();
+
+    if (!fighterOne || !fighterTwo) return;
+
+    this.isFighting.set(true);
+    this.winner.set(null);
+
+    const combatRequest:CombatRequest = {
+      speciesIdOne: fighterOne.id,
+      speciesIdTwo: fighterTwo.id 
+    }
+
+    this.combatsApi.executeSpeciesCombat(combatRequest).subscribe({
+      next: (response) => {
+        this.winner.set(response);
+      },
+      error: (error) => {
+        console.error('Ha ocurrido un error ejecutando el combate: ', error);
+      },
+      complete: () => {
+        this.isFighting.set(false);
+      }
+    });
+  }
+
+  reset():void {
+    this.fighterOne.set(null);
+    this.fighterTwo.set(null);
+    this.winner.set(null);
+  }
+}
