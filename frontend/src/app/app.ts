35adc0314bc1ed9385983d5c71393b472b7da496
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-30 03:58:20 -0400

    feat(frontend): integrate navbar and router outlet into app component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { Navbar } from '@shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Navbar, RouterOutlet],
   templateUrl: './app.html'
 })
 export class App {}
