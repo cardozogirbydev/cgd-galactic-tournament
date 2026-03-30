1dcdbe9cfd60a0d40141db4ae209791c66f29cda
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-30 04:12:42 -0400

    feat(frontend): define application routes for ranking, arena, and species features

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes:Routes = [];
+import { Home } from '@features/home/home';
+
+export const routes:Routes = [
+  {
+    path: 'ranking',
+    loadChildren: () => import('./features/ranking/ranking.routes').then(m => m.routes)
+  },
+  {
+    path: 'arena',
+    loadChildren: () => import('./features/arena/arena.routes').then(m => m.routes)
+  },
+  {
+    path: 'species',
+    loadChildren: () => import('./features/species/species.routes').then(m => m.routes)
+  },
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
