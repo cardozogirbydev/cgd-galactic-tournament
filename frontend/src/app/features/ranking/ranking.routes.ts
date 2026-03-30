3c2f41b6d45108e31a645abee269f77edd86b070
cardozogirbydev <200120836+cardozogirbydev@users.noreply.github.com>
2026-03-30 04:12:37 -0400

    feat(frontend): add routing for ranking list component

---
+++ frontend/src/app/features/ranking/ranking.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { RankingList } from './pages/ranking-list/ranking-list';
+
+export const routes:Routes = [
+  {
+    path: '',
+    component: RankingList
+  }
+];
